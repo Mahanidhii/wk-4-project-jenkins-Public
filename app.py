07765eae886ab7fcb014407e6a0a0a24b6b7eb8d
Mahanidhii <mahanidhi.gk@gmail.com>
2024-10-03 11:06:08 +0530

    edited app file

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-From Flask import Flask 
+from flask import Flask 
 app=Flask(__name__)
 @app.route('/')
-def hello_world();
-     Return 'Hello, Jenkins!'
+def hello_world():
+     return 'Hello, Jenkins!'
 
-If __name__=='__main__'
+if __name__=='__main__':
      app.run(host='0.0.0.0',port=5000)
